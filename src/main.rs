7250ab2cfa7cd0ae4dd0a2975fa211f86949e091
Ratul Maharaj <ratulmaharaj@gmail.com>
2022-08-08 00:27:59 +0200

    Added create_tables

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+mod create_tables;
 mod indexing;
+
+use create_tables::create_tables;
 use indexing::index_folder;
 
 fn main() {
-    let res = index_folder("/Users/ratulmaharaj/Desktop");
+    // Create database and tables if required
+    create_tables().ok();
+    let _res = index_folder("/Users/ratulmaharaj/Desktop");
     // for item in res.iter() {
     //     println!("{:?} \n", item)
     // }
